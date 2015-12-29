b6c15e271c129a4d71b58c99da1d8387fa023285
Raphael Azl <Raphael@macbook-pro-de-raphael-2.local>
2015-12-29 16:02:57 +0200

    Initial Commit

---
+++ hw2w/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw2w
+//
+//  Created by Raphael Azoulay on 29/12/2015.
+//  Copyright © 2015 Raphael Azoulay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
